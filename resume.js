d4d448d7de53401fc8fa5a001d2117f0b08f3c00
Ryan Chen <46980180+ryanjchen@users.noreply.github.com>
2019-02-20 09:20:55 -0800

    Update resume.js

--- resume.js
+++ resume.js
@@ -1,21 +1,25 @@
-/*var slideIndex = 0;
+var slideIndex=0;
 showSlides();
-
 function showSlides() {
     var i;
-    var slides =document.getElementByClassName("mySlides");
-    for (i = 0; i < slides.length; i++) {
+    var slides=document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++){
         slides[i].style.display = "none";
     }
     slideIndex++;
     if (slideIndex > slides.length) {slideIndex=1}
-    slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 2000);
+    slides[slideIndex-1].style.display = "inline";
+    setTimeout(showSlides,7000);
 }
 
-/*function plusSlides(n) {
-    showSlides(slideIndex += n);
+var i = 0;
+var text = " hi there, I'm"
+function typewriter() {
+    if (i <text.length) {
+        document.getElementById("title").innerHTML += text.charAt(i);
+        i++;
+        document.querySelector("h3").innerHTML = text.substring(0, i+1) + '<span class="blinker" aria-hidden="true"></span>';
+        setTimeout(typewriter, 200);
+    }
 }
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}*/
+window.onload=setTimeout(typewriter,3000);
